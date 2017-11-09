181d0b3685f3a3abb1314ade3f854dda5a4c9c8b
JeongsooHa <jeongsoo.ha1210@gmail.com>
2017-11-09 10:35:56 -0800

    Changed while -> for

--- PythonStudy/loadImgFromTextfile.py
+++ PythonStudy/loadImgFromTextfile.py
@@ -4,32 +4,37 @@ import sys
 
 def readAndPrediction(filename):
 	filePath = "/home/spc/Documents/AirSim/"
-	imgTextPath = open(filePath+filename+"/OUTIMAGE.txt","r")
-	imgPath = filePath+filename+"/images/"
-	directionText = open(filePath+filename+"/DIRECTION.txt","w")	
+	activatedPath = filePath+filename
 
+	imgTextPath = open(activatedPath+"/OUTIMAGE.txt","r")
+	directionText = open(activatedPath+"/DIRECTION.txt","w")
+	imgPath = activatedPath+"/images/"
+		
 	
-	#while(True):
-	
-	#imgtext = read a line using imgTextPath
-	imgtext = imgTextPath.readline().splitlines()[0]
-	print(imgtext)
-	
-	#rawimg = load image using imgtext and imgPath
-	print(imgPath+imgtext)
-	rawimg = cv2.imread(imgPath+imgtext,3)	
-	#print(rawimg)
-	cv2.imshow(imgtext,rawimg)
+	for imgtext in imgTextPath:
 
-	#direction = predict(rawimg)
-	#direction = model.predict(rawimg)
-	direction = 0.0001	
-	#cv2.waitKey(0)
-	#cv2.destroyAllWindows()
+		#imgtext = read a line using imgTextPath
+		imgtext = imgtext.splitlines()[0]
+		
+		print(imgtext)
 	
-	#direction.txt << direction
-	directionText.write(str(direction)+"\n")		
+		#rawimg = load image using imgtext and imgPath
+		#print(imgPath+imgtext)
+		rawimg = cv2.imread(imgPath+imgtext,3)	
+		#print(rawimg)
+		#cv2.imshow(imgtext,rawimg)
+
+		#direction = model.predict(rawimg)
+
+		direction = imgtext + str(0.0001) #example	
+		
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
 	
+		#Write direction to DIRECTION.txt
+		directionText.write(str(direction)+"\n")
+
+
 	imgTextPath.close()	
 	directionText.close()
 
